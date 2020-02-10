7fe98fa39e25f4c222ec8e88ce0ec768b513451d
dmfrodol <dmfrodol@yandex.ru>
2020-02-10 06:18:26 -0800

    try to understand how exactly rspec works with rails

--- spec/models/user_profile_spec.rb
+++ spec/models/user_profile_spec.rb
@@ -1,5 +1,6 @@
+require 'rspec'
 require 'rails_helper'
 
-RSpec.describe UserProfile, type: :model do
+describe UserProfile, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,14 @@
 # frozen_string_literal: true
+
 require 'rspec'
 require 'rails_helper'
+# require_relative '../../app/models/user'
 
-describe User, type: :model do
-  context ''
+Rspec.describe User, type: :model do
+  # context 'user management' do
+  #   it 'should select all passed tests for user' do
+  #     # user = User.first
+  #     # user.get_all_passed_tests_with_level(1)
+  #   end
+  # end
 end
