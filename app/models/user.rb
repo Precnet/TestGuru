cbb4db736d93e25cc44dfaee4873f500cf252df4
dmfrodol <dmfrodol@yandex.ru>
2020-02-10 06:17:11 -0800

    add part of code for getting all tests for level

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,14 @@
 class User < ApplicationRecord
+  def get_all_passed_tests_with_level(level)
+    message = 'There is no such test level in Database!'
+    raise ArgumentError, message unless level_exists? level
+
+    p UserProfile.where(user_id: id)
+  end
+
+  private
+
+  def level_exists?(level)
+    Test.level.include?(level)
+  end
 end
