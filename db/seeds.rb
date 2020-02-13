b5702c26588f6ff84fdd764e8156617f4570fc96
dmfrodol <dmfrodol@yandex.ru>
2020-02-13 01:49:45 -0800

    get rid of all manual generation: now almost all values are created via generators with a bit of randomisation all created objects stored in variables for possible further usage

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -6,69 +7,67 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create(name: 'Admin')
-User.create(name: 'Bob')
-User.create(name: 'Alisa')
+# USERS
+user_names = %w[Admin Bob Alisa]
+users = {}
+user_names.each { |name| users[name] = User.create(name: name) }
 
-Category.create(title: 'Programming')
-Category.create(title: 'History')
-Category.create(title: 'Math')
+# CATEGORIES
+categories_names = %w[Programming History Math]
+categories = {}
+categories_names.each { |cat| categories[cat] = Category.create(title: cat) }
 
-Test.create(title: 'Ruby: Basics', level: 1, category_id: 1)
-Test.create(title: 'Ruby: Classes', level: 2, category_id: 1)
-Test.create(title: 'History: Dark Ages', level: 1, category_id: 2)
-Test.create(title: 'Math: Computations', level: 2, category_id: 3)
-Test.create(title: 'Math: High', level: 4, category_id: 3)
+# TESTS
+test_levels = %w[basic middle high]
+tests_data = []
+categories.each_key do |category_name|
+  test_levels.each do |level|
+    test_name = "#{category_name}: #{level}"
+    level_index = test_levels.find_index(level) + 1
+    id = categories[category_name].id
+    tests_data.append(title: test_name, level: level_index, category_id: id)
+  end
+end
+tests = {}
+tests_data.each { |data| tests[data[:title]] = Test.create(data) }
 
-Question.create(body: 'Question 1 for Ruby?', test_id: 1)
-Question.create(body: 'Question 2 for Ruby?', test_id: 1)
-Question.create(body: 'Question 1 for classes in Ruby?', test_id: 2)
-Question.create(body: 'Question 2 for classes in Ruby?', test_id: 2)
-Question.create(body: 'Question 3 for classes in Ruby?', test_id: 2)
-Question.create(body: 'Question 1 for History?', test_id: 3)
-Question.create(body: 'Question 2 for History?', test_id: 3)
-Question.create(body: 'Question 1 for basic Math?', test_id: 4)
-Question.create(body: 'Question 1 for high Math?', test_id: 5)
-Question.create(body: 'Question 2 for high Math?', test_id: 5)
-Question.create(body: 'Question 3 for high Math?', test_id: 5)
+# QUESTIONS
+questions_data = []
+tests.each_key do |test_name|
+  # generate 3 questions for each test
+  (1..3).each do |number|
+    test_id = tests[test_name].id
+    questions_data.append(body: "Question #{number} for " + test_name,
+                          test_id: test_id)
+  end
+end
+questions = {}
+questions_data.each { |data| questions[data[:body]] = Question.create(data) }
 
-Answer.create(body: 'Answer for Q1 Ruby basics', is_correct: true, question_id: 1)
-Answer.create(body: 'Answer for Q1 Ruby basics', is_correct: false, question_id: 1)
-Answer.create(body: 'Answer for Q1 Ruby basics', is_correct: false, question_id: 1)
-Answer.create(body: 'Answer for Q2 Ruby basics', is_correct: true, question_id: 2)
-Answer.create(body: 'Answer for Q2 Ruby basics', is_correct: false, question_id: 2)
-Answer.create(body: 'Answer for Q2 Ruby basics', is_correct: false, question_id: 2)
-Answer.create(body: 'Answer for Q1 Ruby classes', is_correct: true, question_id: 3)
-Answer.create(body: 'Answer for Q1 Ruby classes', is_correct: false, question_id: 3)
-Answer.create(body: 'Answer for Q1 Ruby classes', is_correct: false, question_id: 3)
-Answer.create(body: 'Answer for Q2 Ruby classes', is_correct: true, question_id: 4)
-Answer.create(body: 'Answer for Q2 Ruby classes', is_correct: false, question_id: 4)
-Answer.create(body: 'Answer for Q2 Ruby classes', is_correct: false, question_id: 4)
-Answer.create(body: 'Answer for Q3 Ruby classes', is_correct: true, question_id: 5)
-Answer.create(body: 'Answer for Q3 Ruby classes', is_correct: false, question_id: 5)
-Answer.create(body: 'Answer for Q3 Ruby classes', is_correct: false, question_id: 5)
-Answer.create(body: 'Answer for Q1 History DA', is_correct: true, question_id: 6)
-Answer.create(body: 'Answer for Q1 History DA', is_correct: false, question_id: 6)
-Answer.create(body: 'Answer for Q1 History DA', is_correct: false, question_id: 6)
-Answer.create(body: 'Answer for Q2 History DA', is_correct: true, question_id: 7)
-Answer.create(body: 'Answer for Q2 History DA', is_correct: false, question_id: 7)
-Answer.create(body: 'Answer for Q2 History DA', is_correct: false, question_id: 7)
-Answer.create(body: 'Answer for Q1 Math basic', is_correct: true, question_id: 8)
-Answer.create(body: 'Answer for Q1 Math basic', is_correct: false, question_id: 8)
-Answer.create(body: 'Answer for Q1 Math basic', is_correct: false, question_id: 8)
-Answer.create(body: 'Answer for Q1 Math high', is_correct: true, question_id: 9)
-Answer.create(body: 'Answer for Q1 Math high', is_correct: false, question_id: 9)
-Answer.create(body: 'Answer for Q1 Math high', is_correct: false, question_id: 9)
-Answer.create(body: 'Answer for Q2 Math high', is_correct: true, question_id: 10)
-Answer.create(body: 'Answer for Q2 Math high', is_correct: false, question_id: 10)
-Answer.create(body: 'Answer for Q2 Math high', is_correct: false, question_id: 10)
-Answer.create(body: 'Answer for Q3 Math high', is_correct: true, question_id: 11)
-Answer.create(body: 'Answer for Q3 Math high', is_correct: false, question_id: 11)
-Answer.create(body: 'Answer for Q3 Math high', is_correct: false, question_id: 11)
+# ANSWERS
+answers_data = []
+questions.each_key do |q_name|
+  # generate 3 answers for each question
+  answer_status = [true, false, false].shuffle
+  (1..3).each do |number|
+    answers_data.append(body: "Answer #{number} for #{q_name}",
+                        is_correct: answer_status[number - 1],
+                        question_id: questions[q_name].id)
+  end
+end
+answers = []
+answers_data.each { |data| answers.append(Answer.create(data)) }
 
-UserProfile.create(user_id: 2, test_id: 1, test_status: 'passed')
-UserProfile.create(user_id: 2, test_id: 2, test_status: 'failed')
-UserProfile.create(user_id: 2, test_id: 3, test_status: 'passed')
-UserProfile.create(user_id: 3, test_id: 3, test_status: 'passed')
-UserProfile.create(user_id: 3, test_id: 4, test_status: 'passed')
-UserProfile.create(user_id: 3, test_id: 5, test_status: 'passed')
+# USER`S PROFILES
+user_profiles = []
+users.each_key do |name|
+  # generate 3 random tests taken per user: can be either passes of failed
+  # user can take single test multiple times with different results - why not
+  3.times do
+    test_id = tests[tests.keys.sample].id
+    status = %w[passed failed].sample
+    user_profiles.append(UserProfile.create(user_id: users[name].id,
+                                            test_id: test_id,
+                                            test_status: status))
+  end
+end
