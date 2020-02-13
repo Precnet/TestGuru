5ce4199a516fa544e5bf2117d7f3fd36328f61a6
dmfrodol <dmfrodol@yandex.ru>
2020-02-13 04:15:00 -0800

    fix tests according to new db structure

--- spec/models/test_spec.rb
+++ spec/models/test_spec.rb
@@ -11,9 +11,9 @@ describe Test, type: :model do
     # end
     it 'should return list of tests for category' do
       expect { Test.get_test_by_category('History') }.not_to raise_error
-      result_math = ['Math: High', 'Math: Computations']
+      result_math = ['Math: middle', 'Math: high', 'Math: basic']
       expect(Test.get_test_by_category('Math')).to eq(result_math)
-      result_programming = ['Ruby: Classes', 'Ruby: Basics']
+      result_programming = ['Programming: middle', 'Programming: high', 'Programming: basic']
       expect(Test.get_test_by_category('Programming')).to eq(result_programming)
     end
   end
