9817a6f30f6b9acaf8523e40446fb035630cc773
dmfrodol <dmfrodol@yandex.ru>
2020-02-14 00:49:27 -0800

    make request via Test table

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,9 @@ require_relative 'test'
 
 class User < ApplicationRecord
   def get_passed_tests_by(level)
-    query = 'join user_profiles on users.id = user_profiles.user_id
-              join tests on user_profiles.test_id = tests.id'
-    User.joins(query)
-        .where(tests: { level: level })
-        .where(users: { id: id })
+    query = 'join user_profiles on tests.id = user_profiles.test_id'
+    Test.joins(query)
+        .where(level: level, user_profiles: { user_id: id })
         .pluck(:title)
   end
 end
