9ce6e066bb1086fe4ebcf69495f68a852f1dd949
dmfrodol <dmfrodol@yandex.ru>
2020-02-14 00:40:22 -0800

    remove test of excess value check

--- spec/models/test_spec.rb
+++ spec/models/test_spec.rb
@@ -7,7 +7,6 @@ require_relative '../../app/models/test'
 describe Test, type: :model do
   context 'test management' do
     it 'should return list of tests for category' do
-      expect { Test.get_test_by_category('History') }.not_to raise_error
       result_math = ['Math: middle', 'Math: high', 'Math: basic']
       expect(Test.get_test_by_category('Math')).to eq(result_math)
       result_programming = ['Programming: middle', 'Programming: high', 'Programming: basic']
