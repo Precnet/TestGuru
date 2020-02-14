8af1d59a2ffbc91e48c470a77746558e67d115f6
dmfrodol <dmfrodol@yandex.ru>
2020-02-14 00:34:25 -0800

    remove excess value check

--- app/models/test.rb
+++ app/models/test.rb
@@ -4,19 +4,8 @@ require_relative 'category'
 
 class Test < ApplicationRecord
   def self.get_test_by_category(category_name)
-    message = "There is no category '#{category_name}' in Database!"
-    raise ArgumentError, message unless category_exists?(category_name)
-
     query = 'join categories on tests.category_id = categories.id where categories.title = ? order by tests.title desc'
     sanitized_query = sanitize_sql_array([query, category_name])
     joins(sanitized_query).map(&:title)
-  end
-
-  def self.category_exists?(category)
-    category_occurrences = Category.where(title: category).count
-    message = "More than one instance of category: '#{category}' in DB!"
-    raise ArgumentError, message if category_occurrences > 1
-
-    category_occurrences.nonzero?
   end
 end
