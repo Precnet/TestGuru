ae6d87767066a966ba759fb2c69c72f0558e33fc
dmfrodol <dmfrodol@yandex.ru>
2020-02-14 00:40:57 -0800

    use AR methods -> sanitizing inside of 'where' block

--- app/models/test.rb
+++ app/models/test.rb
@@ -4,8 +4,9 @@ require_relative 'category'
 
 class Test < ApplicationRecord
   def self.get_test_by_category(category_name)
-    query = 'join categories on tests.category_id = categories.id where categories.title = ? order by tests.title desc'
-    sanitized_query = sanitize_sql_array([query, category_name])
-    joins(sanitized_query).map(&:title)
+    pp joins('join categories on tests.category_id = categories.id')
+      .where(categories: { title: category_name })
+      .order(title: :desc)
+      .pluck(:title)
   end
 end
