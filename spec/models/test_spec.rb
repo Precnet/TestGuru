d4cb9ebb5fa99c1b1bb96c50cb1270d4de2e48c4
dmfrodol <dmfrodol@yandex.ru>
2020-02-14 00:35:02 -0800

    remove test of excess value check

--- spec/models/test_spec.rb
+++ spec/models/test_spec.rb
@@ -6,9 +6,6 @@ require_relative '../../app/models/test'
 
 describe Test, type: :model do
   context 'test management' do
-    it 'should raise an error for unexistant category' do
-      expect { Test.get_test_by_category('Art') }.to raise_error(ArgumentError)
-    end
     it 'should return list of tests for category' do
       expect { Test.get_test_by_category('History') }.not_to raise_error
       result_math = ['Math: middle', 'Math: high', 'Math: basic']
