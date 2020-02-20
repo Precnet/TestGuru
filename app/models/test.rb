8632ae387817064ae8305b70cfcd78cfe7477510
dmfrodol <dmfrodol@yandex.ru>
2020-02-19 23:48:20 -0800

    add association test-user through user_profile

--- app/models/test.rb
+++ app/models/test.rb
@@ -4,6 +4,8 @@ require_relative 'category'
 
 class Test < ApplicationRecord
   has_many :questions
+  has_many :user_profiles
+  has_many :users, through: :user_profiles
   belongs_to :category
   def self.get_test_by_category(category_name)
     pp joins('join categories on tests.category_id = categories.id')
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 require_relative 'test'
 
 class User < ApplicationRecord
+  has_many :user_profiles
+  has_many :tests, through: :user_profiles
   def get_passed_tests_by(level)
     query = 'join user_profiles on tests.id = user_profiles.test_id'
     Test.joins(query)
