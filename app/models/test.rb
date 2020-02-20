6bbeee5e3620aadf87039b51e7b01d90d60aba0a
dmfrodol <dmfrodol@yandex.ru>
2020-02-19 23:41:12 -0800

    add association test-question

--- app/models/test.rb
+++ app/models/test.rb
@@ -3,6 +3,7 @@
 require_relative 'category'
 
 class Test < ApplicationRecord
+  has_many :questions
   def self.get_test_by_category(category_name)
     pp joins('join categories on tests.category_id = categories.id')
       .where(categories: { title: category_name })
