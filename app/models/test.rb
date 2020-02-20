e9fb8301d3ba4c415a86a9e1458c068ca00fb650
dmfrodol <dmfrodol@yandex.ru>
2020-02-20 00:18:47 -0800

    remove duplicated line

--- app/models/test.rb
+++ app/models/test.rb
@@ -10,7 +10,6 @@ class Test < ApplicationRecord
   belongs_to :user, foreign_key: 'author'
   def self.get_test_by_category(category_name)
     pp joins('join categories on tests.category_id = categories.id')
-    pp joins('join categories on tests.category_id = categories.id')
       .where(categories: { title: category_name })
       .order(title: :desc)
       .pluck(:title)
