8bbde9e57a25f145ccd128d630f06fdac5f90f7b
dmfrodol <dmfrodol@yandex.ru>
2020-02-20 00:02:38 -0800

    add index to author

--- db/migrate/20200220074948_add_author_to_test.rb
+++ db/migrate/20200220074948_add_author_to_test.rb
@@ -1,5 +1,5 @@
 class AddAuthorToTest < ActiveRecord::Migration[6.0]
   def change
-    add_column(:tests, 'author', :integer, null: false)
+    add_column(:tests, 'author', :integer, null: false, index: true)
   end
 end
