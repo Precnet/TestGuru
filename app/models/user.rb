b3d352d6f3560e702ba8ba582bdd8a9c323ed739
dmfrodol <dmfrodol@yandex.ru>
2020-02-26 05:04:55 -0800

    implement join through association

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ApplicationRecord
   has_many :user_profiles
   has_many :tests, through: :user_profiles
   def get_passed_tests_by(level)
-    query = 'join user_profiles on tests.id = user_profiles.test_id'
-    Test.joins(query)
+    # query = 'join user_profiles on tests.id = user_profiles.test_id'
+    Test.joins(:user_profiles)
         .where(level: level, user_profiles: { user_id: id })
         .pluck(:title)
   end
