49697f1a4ed2f205000160df7ca038cda61d982c
dmfrodol <dmfrodol@yandex.ru>
2020-02-26 05:28:31 -0800

    simplify request

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,7 @@ class User < ApplicationRecord
   has_many :tests, through: :user_profiles
   def get_passed_tests_by(level)
     # query = 'join user_profiles on tests.id = user_profiles.test_id'
-    Test.joins(:user_profiles)
-        .where(level: level, user_profiles: { user_id: id })
+    Test.where(level: level)
         .pluck(:title)
   end
 end
