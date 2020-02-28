5e59e4762d082eef7ad26dd1b1f7744d37f76658
dmfrodol <dmfrodol@yandex.ru>
2020-02-28 00:48:50 -0800

    add tests for getting list of tests by difficulty

--- spec/models/test_spec.rb
+++ spec/models/test_spec.rb
@@ -12,5 +12,14 @@ describe Test, type: :model do
       result_programming = ['Programming: middle', 'Programming: high', 'Programming: basic']
       expect(Test.get_test_by_category('Programming')).to eq(result_programming)
     end
+    it 'should return easy tests (level: 0..1)' do
+      expect(Test.easy).to eq(Test.where(level: 0..1))
+    end
+    it 'should return standard tests (level: 2..4)' do
+      expect(Test.standard).to eq(Test.where(level: 2..4))
+    end
+    it 'should return difficult tests (level: 5..Float::INFINITY)' do
+      expect(Test.difficult).to eq(Test.where(level: 5..Float::INFINITY))
+    end
   end
 end
