16d60dc61ca4ecf7ebd47f13035f58e526ebe3e7
dmfrodol <dmfrodol@yandex.ru>
2020-02-28 01:11:57 -0800

    replace model method with scope method

--- app/models/test.rb
+++ app/models/test.rb
@@ -11,11 +11,10 @@ class Test < ApplicationRecord
   scope :easy, -> { where(level: 0..1) }
   scope :standard, -> { where(level: 2..4) }
   scope :difficult, -> { where(level: 5..Float::INFINITY) }
-
-  def self.get_test_by_category(category_name)
-    pp joins(:category)
+  scope :by_category, lambda { |category_name|
+    joins(:category)
       .where(categories: { title: category_name })
       .order(title: :desc)
       .pluck(:title)
-  end
+  }
 end
