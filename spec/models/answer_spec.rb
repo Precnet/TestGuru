afe5b6956c6feafe635aaba2660ea95da7776690
dmfrodol <dmfrodol@yandex.ru>
2020-02-28 02:40:53 -0800

    add body verification + tests

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -5,6 +5,14 @@ require 'rails_helper'
 require_relative '../../app/models/answer'
 
 describe Answer, type: :model do
+  context 'verification' do
+    it 'should verify body column' do
+      expect(Answer.new.valid?).to eq(false)
+      expect(Answer.new(body: 'answer',
+                        is_correct: false,
+                        question_id: Question.all.map(&:id).sample).valid?).to eq(true)
+    end
+  end
   context 'answers management' do
     it 'return correct answers' do
       expect(Answer.correct.to_a.all? { |answer| answer['is_correct'] }).to eq(true)
