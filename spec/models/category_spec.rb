5fda1117caf5e982e2e4492a628aa9d2ce1126d0
dmfrodol <dmfrodol@yandex.ru>
2020-02-28 02:42:52 -0800

    add title verification + tests

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -5,6 +5,12 @@ require 'rails_helper'
 require_relative '../../app/models/category'
 
 describe Category, type: :model do
+  context 'validation' do
+    it 'should validate title' do
+      expect(Category.new.valid?).to eq(false)
+      expect(Category.new(title: 'new category').valid?).to eq(true)
+    end
+  end
   context 'category management' do
     it 'should by default return list af categories in ascending order' do
       categories = Category.all.map(&:title).insert(0, 'Aviation').insert(-1, 'Zen')
