b5802ccd5c5cd35ba3875a446b1f0fb397c7cc49
dmfrodol <dmfrodol@yandex.ru>
2020-02-28 04:17:17 -0800

    add verification for question to have no more than 4 answers + tests

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,5 +1,14 @@
 class Answer < ApplicationRecord
   belongs_to :question
   validates :body, presence: true
+  validate :validate_amount_per_question
   scope :correct, -> { where(is_correct: true) }
+
+  private
+
+  def validate_amount_per_question
+    if Answer.where(question_id: question_id).count >= 4
+      errors.add(:question_id, 'Too many answers!')
+    end
+  end
 end
--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -12,6 +12,11 @@ describe Answer, type: :model do
                         is_correct: false,
                         question_id: Question.all.map(&:id).sample).valid?).to eq(true)
     end
+    it 'should verify that there are no questions with more than 4 answers' do
+      question_id = Answer.all.map(&:question_id).sample
+      Answer.new(body: 'good', is_correct: 'false', question_id: question_id).save
+      expect(Answer.new(body: 'bad', is_correct: 'false', question_id: question_id).valid?).to eq(false)
+    end
   end
   context 'answers management' do
     it 'return correct answers' do
