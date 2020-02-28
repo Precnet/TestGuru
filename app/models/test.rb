06802b318c670080ff7ddafc44e14ffb4a07f394
dmfrodol <dmfrodol@yandex.ru>
2020-02-28 00:48:59 -0800

    add code for getting list of tests by difficulty

--- app/models/test.rb
+++ app/models/test.rb
@@ -8,6 +8,10 @@ class Test < ApplicationRecord
   has_many :users, through: :user_profiles
   belongs_to :category
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+  scope :easy, -> { where(level: 0..1) }
+  scope :standard, -> { where(level: 2..4) }
+  scope :difficult, -> { where(level: 5..Float::INFINITY) }
+
   def self.get_test_by_category(category_name)
     pp joins(:category)
       .where(categories: { title: category_name })
