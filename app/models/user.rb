a369137c58b94fa69ab450f6bd3fc23830569700
dmfrodol <dmfrodol@yandex.ru>
2020-02-28 02:33:05 -0800

    add email verification + tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :user_profiles
   has_many :tests, through: :user_profiles
+  validates :email, presence: true
   def get_passed_tests_by(level)
     tests.by_level(level)
   end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,13 +5,20 @@ require 'rails_helper'
 require_relative '../../app/models/user'
 
 describe User, type: :model do
+  context 'user creation' do
+    it 'should validate email presence' do
+      expect(User.new.valid?).to eq(false)
+      expect(User.new(name: '123').valid?).to eq(false)
+      expect(User.new(name: 'Some name', email: '123.email').valid?).to eq(true)
+    end
+  end
   context 'user management' do
     it 'should select all passed tests for user' do
-      user_bob = User.find(2)
-      expect(user_bob.get_passed_tests_by(1)).to be_a_kind_of(Array)
-      user_alisa = User.find(3)
-      expect(user_alisa.get_passed_tests_by(1)).to be_a_kind_of(Array)
-      expect(user_alisa.get_passed_tests_by(3)).to be_a_kind_of(Array)
+      user_bob = User.all.sample
+      expect(user_bob.get_passed_tests_by(Test.all.map(&:level).sample)).to be_a_kind_of(Array)
+      user_alisa = User.all.sample
+      expect(user_alisa.get_passed_tests_by(Test.all.map(&:level).sample)).to be_a_kind_of(Array)
+      expect(user_alisa.get_passed_tests_by(Test.all.map(&:level).sample)).to be_a_kind_of(Array)
     end
   end
 end
