8b16bbb50ec1396f543a1efde20451eb7bf2673f
dmfrodol <dmfrodol@yandex.ru>
2020-03-02 06:13:18 -0800

    move pluck invocation out of scope-method

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ApplicationRecord
   has_many :user_profiles
   has_many :tests, through: :user_profiles
   validates :email, presence: true
+
   def get_passed_tests_by(level)
-    tests.by_level(level)
+    tests.by_level(level).pluck(:title)
   end
 end
